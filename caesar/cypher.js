26478db203f860f803479b22091d216b6cd0fa8d
ruttj1 <76194219+ruttj1@users.noreply.github.com>
2022-04-03 13:52:52 -0400

    applies logic to input text

--- caesar/cypher.js
+++ caesar/cypher.js
@@ -3,37 +3,39 @@ function refresh() {
 	window.location.reload(); 
 }
 
-// validate plainText is not empty
-function checkPlaintext () {
-	// if plainText is null
-	if (document.getElementById("plainText").value === null) { 
-		// alert to input value
-		alert("Please provide text to encrypt!"); 
-		// refresh page
-		refresh(); 	
-	}
-}
-
-// validate the value in the key field is a number and exists
-function checkKey () {
-	// if value is not a number
-	if (!document.getElementById("key").value || isNaN(document.getElementById("key")) {
-		// alert to input number
-		alert("Please provide a key!"); 
-		// refresh page 
-		refresh(); 
-	}  
-}
-
 // write cypher function
-function cypher (plainText, key) {
-	checkPlaintext (); 
-	checkKey (); 
-// iterate over each letter of plaintext
-	// if it is a letter between a and z or A and Z
-		// change the letter by the number provided in key
-		// store the value in a new array
-	// if it is not a letter, copy the value into the new array 
-// display the contents of the new array 
+function encrypt(plainText, key) {
+	var key = key % 26; 
+	var temp = []; 
+	var cypherText;  
+	// iterate over each letter of plaintext
+	for (var i = 0; i < plainText.length; i++) { 
+		// if it is a letter between a and z or A and Z
+		if (plainText[i] >= 'a' && plainText[i] <= 'z') { 
+			
+			// change the letter by the number provided in key
+			var code = plainText.charCodeAt(i) - 97;  	
+			code = code + key;
+			code = code + 97;
+		
+			// store the value in a new array
+			temp.push(code);		
+		} else if (plainText[i] >= 'A' && plainText[i] <= 'Z') { 
+			var code = plainText.charCodeAt(i) - 65;
+			code = code + key;
+			code = code + 65; 
+		
+			// store the value in array
+			temp.push(code); 
+		} else { 
+		// if it is not a letter, copy the value into the new array 
+		temp.push(plainText[i]); 
+		} 
+	} 
+	console.log(temp); 
+	// display the contents of the new array 
+	cypherText = String.fromCharCode.apply(null, temp); 
+
+	document.getElementById("cypherText").innerHTML = cypherText; 
 }
 
