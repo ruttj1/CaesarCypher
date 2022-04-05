df985b18fe56d2bef6d50e70d9f88f815c824b4c
ruttj1 <76194219+ruttj1@users.noreply.github.com>
2022-04-04 21:16:23 -0400

    fix bug of non letters being included

--- caesar/cypher.js
+++ caesar/cypher.js
@@ -5,7 +5,7 @@ function refresh() {
 
 // write cypher function
 function encrypt(plainText, key) {
-	var key = key % 26; 
+	var k = key % 26; 
 	var temp = []; 
 	var cypherText;  
 	// iterate over each letter of plaintext
@@ -14,22 +14,17 @@ function encrypt(plainText, key) {
 		if (plainText[i] >= 'a' && plainText[i] <= 'z') { 
 			
 			// change the letter by the number provided in key
-			var code = plainText.charCodeAt(i) - 97;  	
-			code = code + key;
-			code = code + 97;
-		
+			var code = (((plainText.charCodeAt(i) - 97) + k) % 26) + 97;  	
+				
 			// store the value in a new array
 			temp.push(code);		
 		} else if (plainText[i] >= 'A' && plainText[i] <= 'Z') { 
-			var code = plainText.charCodeAt(i) - 65;
-			code = code + key;
-			code = code + 65; 
-		
+			var code = (((plainText.charCodeAt(i) - 65) + k) %26) + 65;
 			// store the value in array
 			temp.push(code); 
 		} else { 
 		// if it is not a letter, copy the value into the new array 
-		temp.push(plainText[i]); 
+		temp.push(plainText.charCodeAt(i)); 
 		} 
 	} 
 	console.log(temp); 
